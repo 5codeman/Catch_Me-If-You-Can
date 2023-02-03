5452caa583ef98723591e072585c29a633f24c93
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-02-03 12:19:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 alert("Touch the Box and see that you are able to catch the box or not \n बॉक्स को स्पर्श करें और देखें कि आप बॉक्स को पकड़ पा रहे हैं या नहीं");
 let Box = document.getElementById("Box");
 let Box_height = Box.clientHeight;
