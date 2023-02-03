9215b7aa73aaf8ff68c57f05597522bde6490795
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2023-02-03 11:29:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+// alert("Touch the Box and see that you are able to catch the box or not \n बॉक्स को स्पर्श करें और देखें कि आप बॉक्स को पकड़ पा रहे हैं या नहीं");
+let Box = document.getElementById("Box");
+let Box_height = Box.clientHeight;
+let Box_width = Box.clientWidth;
+Box.addEventListener("mousemove", catchme);
+function catchme() {
+  Box.style.top =
+    Math.floor(Math.random() * (window.innerHeight - Box_height - 6)) + "px";
+  Box.style.left =
+    Math.floor(Math.random() * (window.innerWidth - Box_width - 6)) + "px";
+}
